b86d3e1a3ba2ea570a95502d8b968de41e260b0b
Michal Frystacky <MFry@users.noreply.github.com>
2016-02-28 22:36:12 -0900

    Setting up virtualenv for python3

--- tournament/provision.sh
+++ tournament/provision.sh
@@ -1,4 +1,7 @@
 apt-get -qqy update
+virtualenv -p /usr/bin/python3 py3env
+source py3env/bin/activate
+pip install package-name
 apt-get -qqy install postgresql python3-psycopg2
 apt-get -qqy install python3-pip
 apt-get -qqy install python3-flask python3-sqlalchemy
