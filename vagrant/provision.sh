cf1b7d2433a49e1be1795260c3f490375306667e
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-03-02 18:02:32 -0900

    Updated to reflect usage of requirements.txt

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,10 +1,9 @@
 apt-get -qqy update
+apt-get -qqy install postgresql
 virtualenv -p /usr/bin/python3 py3env
 source py3env/bin/activate
-pip install package-name
-apt-get -qqy install postgresql python3-psycopg2
-apt-get -qqy install python3-pip
-apt-get -qqy install python3-flask python3-sqlalchemy
+cd /vagrant/tournament
+pip install -r requirements.txt
 
 vagrantTip="[35m[1mThe default synced directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
