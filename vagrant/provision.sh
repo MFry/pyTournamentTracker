e8f96f5ab36db8b482f212a49935b6809507f0cf
Michal Frystacky <MFry@users.noreply.github.com>
2016-03-02 17:36:27 -0900

    Update provision.sh
    Adding similarity to original vagrant box.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -5,3 +5,6 @@ pip install package-name
 apt-get -qqy install postgresql python3-psycopg2
 apt-get -qqy install python3-pip
 apt-get -qqy install python3-flask python3-sqlalchemy
+
+vagrantTip="[35m[1mThe default synced directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+echo -e $vagrantTip > /etc/motd
