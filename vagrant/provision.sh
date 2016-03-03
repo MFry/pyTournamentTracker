74fab01c38b60253262126d05080e1a2b6a8c150
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-03-02 18:47:36 -0900

    Updating to allow pip install of requirements

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,9 +1,12 @@
 apt-get -qqy update
 apt-get -qqy install postgresql
-virtualenv -p /usr/bin/python3 py3env
+apt-get -qqy install python3
+apt-get -qqy install python-virtualenv
+apt-get install libpq-dev python3-dev
+sudo -u vagrant virtualenv -p /usr/bin/python3 py3env
 source py3env/bin/activate
 cd /vagrant/tournament
-pip install -r requirements.txt
+sudo -u vagrant pip install -r requirements.txt
 
 vagrantTip="[35m[1mThe default synced directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
