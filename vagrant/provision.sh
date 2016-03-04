39a71c2f769eaf23dcde4bb093f4d17f07701620
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-03-03 18:00:27 -0900

    Setting up provisions.sh to install python 3.5 and pip latest

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,12 +1,23 @@
-apt-get -qqy update
-apt-get -qqy install postgresql
-apt-get -qqy install python3
-apt-get -qqy install python-virtualenv
-apt-get install libpq-dev python3-dev
-sudo -u vagrant virtualenv -p /usr/bin/python3 py3env
-source py3env/bin/activate
-cd /vagrant/tournament
-sudo -u vagrant pip install -r requirements.txt
+apt-get -qq update
+apt-get -qq install postgresql
+# echo
+apt-get -qq install python-virtualenv
+apt-get -qq install libpq-dev libreadline-dev libsqlite3-dev libssl-dev
+# python3-dev
+# apt-get -qq install build-essential checkinstall
+cd /usr/src
+wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz
+sudo tar xzf Python-3.5.1.tgz
+cd python-5.5.1
+sudo ./configure
+sudo make altinstall
+# vsudo -u vagrant virtualenv -p /usr/bin/python3 py3env
+# source py3env/bin/activate
+# cd /vagrant/tournament
+# sudo -u vagrant pip install -r requirements.txt
 
 vagrantTip="[35m[1mThe default synced directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
+#Ref: https://gist.github.com/dwayne/87f807f0d313b444bb37
+#Ref: http://tecadmin.net/install-python-3-4-on-ubuntu-and-linuxmint/#
+#Ref: http://stackoverflow.com/questions/28253681/you-need-to-install-postgresql-server-dev-x-y-for-building-a-server-side-extensi
