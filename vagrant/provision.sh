ede7d9ee6741ba0789f906320e6d807746255046
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-03-10 21:49:47 -0900

    Switched config box and isolated os specific provisions

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,47 +1,7 @@
 #!/usr/bin/env bash
-apt-get -qq update
-sudo -i
-sudo apt-get -qq install synaptic libc6
-#apt-get -qq install postgresql
-# Remove any older postgresql installation you may have (ex:)
-# apt-get -qq remove postgresql postgresql-contrib postgresql-client
 
-touch /etc/apt/sources.list.d/pgdg.list
-echo 'deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main' >> /etc/apt/sources.list.d/pgdg.list
-sudo apt-get install wget ca-certificateswget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-# Install the PostgreSQL
-apt-get -qq install postgresql
 apt-get -qq update
 
-# Create the user to access the db. (vagrant sample)
-sudo -u postgres psql -c "CREATE USER vagrant WITH SUPERUSER CREATEDB ENCRYPTED PASSWORD 'vagrant'"
-# echo
-apt-get -qq install python-virtualenv
-apt-get -qq install libpq-dev libreadline-dev libsqlite3-dev libssl-dev
-# python3-dev
-apt-get -qq install build-essential checkinstall
-cd /tmp
-wget -O- -q https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz | tar xz
-cd Python-3.5.1
-./configure
-make -s
-make -s altinstall
-sudo -H pip3.5 install --upgrade pip
-sudo -H pip3.5 install virtualenvwrapper
-virtualenv --no-site-packages /vagrant/tournament/virtualenv/
-su postgres -c 'createuser -dRS vagrant'
-su vagrant -c 'createdb'
-su vagrant -c 'createdb tournament'
-su vagrant -c 'psql tournament -f /vagrant/tournament/tournament.sql'
-
-#source /vagrant/tournament/virtualenv/bin/activate
-#cd /vagrant/tournament/
-#sudo -H pip install -r requirements.txt
-#sudo -u vagrant virtualenv -p /usr/bin/python3 py3env
-#source py3env/bin/activate
-
-# cd /vagrant/tournament
-# sudo -u vagrant pip install -r requirements.txt
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
