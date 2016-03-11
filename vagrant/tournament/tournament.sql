96aac383eeae5cc2c55b73d59c80a9dab738f962
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-03-11 14:48:51 -0900

    Initial tables setup for the swedish tournament system

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,20 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE players (id SERIAL PRIMARY KEY,
+                      name TEXT);
 
+-- VIEW FOR PLAYER PARTICULAR TOURNAMENT AND THEIR STATS
+
+-- TOURNAMENT : PAIR UP PLAYERS
+CREATE TABLE tournaments (
+  id SERIAL PRIMARY KEY ,
+  name TEXT);
+
+CREATE TABLE tournament (
+  td INTEGER REFERENCES tournaments(id),
+  match SERIAL,
+  player_1 INTEGER REFERENCES players(id),
+  player_2 INTEGER REFERENCES players(id),
+  winner INTEGER REFERENCES players(id),
+  PRIMARY KEY (td, match));
