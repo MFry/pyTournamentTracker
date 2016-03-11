e07385e60124a3dfd905dd63496d6d9bd7092363
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-03-10 16:31:40 -0900

    Makefile instructions and attempting to install postgresql 3.5

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Python module that uses the PostgreSQL database to keep track of players and mat
 #### Python dependencies
 
 ``` bash
+make -f Makefile
+```
+
+``` bash
 source /vagrant/tournament/virtualenv/bin/activate
 cd /vagrant/tournament/
 sudo -H pip install -r requirements.txt
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
-
 apt-get -qq update
+sudo -i
+sudo apt-get -qq install synaptic libc6
+#apt-get -qq install postgresql
+# Remove any older postgresql installation you may have (ex:)
+# apt-get -qq remove postgresql postgresql-contrib postgresql-client
+
+touch /etc/apt/sources.list.d/pgdg.list
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main' >> /etc/apt/sources.list.d/pgdg.list
+sudo apt-get install wget ca-certificateswget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# Install the PostgreSQL
 apt-get -qq install postgresql
+apt-get -qq update
+
+# Create the user to access the db. (vagrant sample)
+sudo -u postgres psql -c "CREATE USER vagrant WITH SUPERUSER CREATEDB ENCRYPTED PASSWORD 'vagrant'"
 # echo
 apt-get -qq install python-virtualenv
 apt-get -qq install libpq-dev libreadline-dev libsqlite3-dev libssl-dev
 # python3-dev
 apt-get -qq install build-essential checkinstall
 cd /tmp
-wget -O- https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz | tar xz
+wget -O- -q https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz | tar xz
 cd Python-3.5.1
 ./configure
-make
-make altinstall
+make -s
+make -s altinstall
 sudo -H pip3.5 install --upgrade pip
 sudo -H pip3.5 install virtualenvwrapper
 virtualenv --no-site-packages /vagrant/tournament/virtualenv/
