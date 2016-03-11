9a8c0c60f06eec84e735fbd431bb61e9d4a100c0
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-03-10 21:45:13 -0900

    Isolated provisions for python 3.5

---
+++ vagrant/python_provisions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# echo
+apt-get -qq install python-virtualenv
+apt-get -qq install libpq-dev libreadline-dev libsqlite3-dev libssl-dev
+# python3-dev
+apt-get -qq install build-essential checkinstall
+cd /tmp
+wget -O- -q https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz | tar xz
+cd Python-3.5.1
+./configure
+make -s
+make -s altinstall
+sudo -H pip3.5 install --upgrade pip
+sudo -H pip3.5 install virtualenvwrapper
+virtualenv --no-site-packages /vagrant/tournament/virtualenv/
