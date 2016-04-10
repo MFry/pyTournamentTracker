2f841e5639a63b86517ada29efb0e3dc6520f89c
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-04-09 23:21:19 -0800

    Corrected comment return of plays

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -208,7 +208,7 @@ def swissPairings(tournament='default'):
                 plays[player] = plays[player].union(set(current_game)) - {player}
             game += 1
             current_game = [match[0]]
-    # plays returns : {1: {1, 2, 4}, 2: {1, 2}, 3: {3, 4}, 4: {1, 3, 4}}
+    # plays returns : {1: {2, 4}, 2: {1}, 3: {4}, 4: {1, 3}}
 
 
     '''
